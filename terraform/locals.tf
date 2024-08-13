052427eb9a7c4600aaea11ad229310be5c9d58ef
Gre4kas <misaa3818@gmail.com>
2024-08-13 14:32:42 +0300

    Fix tf files

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -8,11 +8,11 @@ resource "random_pet" "bucket_suffix" {
 
 locals {
   content_types = {
-    ".html"  : "text/html",
-    ".css"   : "text/css",
-    ".js"    : "application/javascript",
+    ".html" : "text/html",
+    ".css" : "text/css",
+    ".js" : "application/javascript",
     ".woff2" : "font/woff2",
-    ".png"   : "image/png",
-    ".min.js": "application/javascript"
+    ".png" : "image/png",
+    ".min.js" : "application/javascript"
   }
 }
